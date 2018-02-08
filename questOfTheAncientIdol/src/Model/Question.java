a345d450bc665a10f26ab6825f12288fc9579d1f
Natalie Gibson <35414098+natalierae325@users.noreply.github.com>
2018-02-08 12:27:07 -0700

    2/8/18 initial proj

--- questOfTheAncientIdol/src/Model/Question.java
+++ questOfTheAncientIdol/src/Model/Question.java
@@ -10,12 +10,63 @@ import java.util.Objects;
 
 public class Question implements Serializable{
     
-    private String question();
-    private String answer();
+    private String question;
+    private String answer;
     
-    public Question(){
-        
-        
-        
+    @Override
+    public String toString() {
+        return "Question{" + "question=" + question + ", answer=" + answer + '}';
+    }
+    
+
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.question);
+        hash = 79 * hash + Objects.hashCode(this.answer);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Question other = (Question) obj;
+        if (!Objects.equals(this.question, other.question)) {
+            return false;
+        }
+        if (!Objects.equals(this.answer, other.answer)) {
+            return false;
+        }
+        return true;
     }
 }
+    
