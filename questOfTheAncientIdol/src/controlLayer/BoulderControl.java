8b00f2f4c7063ac2a9beb6aaf6b3ae7af0c78629
Natalie Gibson <35414098+natalierae325@users.noreply.github.com>
2018-02-17 21:08:12 -0700

    2/17/18

---
+++ questOfTheAncientIdol/src/controlLayer/BoulderControl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlLayer;
+
+/**
+ *
+ * @author natal
+ */
+public class BoulderControl {
+
+    public static double calcBoulderVolume(double boulderRadius) {
+        if (boulderRadius < 0 || boulderRadius > 15) {
+            return -1;
+        }
+
+        double boulderVolume = (4.0 / 3.0) * 3.14 * Math.pow(boulderRadius, 2);
+        return boulderVolume;
+
+    }
+    
+    public static double calcBoulderSlopeDistance(double boulderSlopeDistance, double slopeDistance){
+        if (boulderSlopeDistance < 0 || boulderSlopeDistance > 35) {
+            return -1;
+        }
+        
+        slopeDistance = Math.pow(9, 2) + Math.pow(16, 2) + Math.pow(slopeDistance, 2);
+            return slopeDistance;
+    }
+}
