8f53e86249d240303d7b357f21410504adc2a97e
abbeymj <35237112+abbeymj@users.noreply.github.com>
2018-03-04 20:38:59 -0700

    MainMenuView end user stories comments

---
+++ questOfTheAncientIdol/src/byui/cit260/questOfTheAncientIdol/view/HelpMenuView.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.questOfTheAncientIdol.view;
+
+/**
+ *
+ * @author abbeymj
+ */
+public class HelpMenuView {
+    
+    /* Q - Quit
+    ** P - Purpose of the game
+    ** I - Inventory/Items throughout game
+    ** T - Traveling in the board (How to play)
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
+    
+    
+}
