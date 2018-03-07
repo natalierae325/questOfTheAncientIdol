0502faa4b1b67520a9ff8421854b78e67deb89bd
Natalie Gibson <35414098+natalierae325@users.noreply.github.com>
2018-03-06 23:32:54 -0700

    start program view test run 3/6/18

--- questOfTheAncientIdol/src/byui/cit260/questOfTheAncientIdol/view/MainMenuView.java
+++ questOfTheAncientIdol/src/byui/cit260/questOfTheAncientIdol/view/MainMenuView.java
@@ -10,8 +10,42 @@ package byui.cit260.questOfTheAncientIdol.view;
  * @author natal
  */
 public class MainMenuView {
-    
-    
+       String displayMessage = ("\n *** Choose an option. ***"); 
+    public void displayMainMenuView() {
+        String[] inputs = new String[1];
+        boolean endOfView = false;
+        do {
+            String value = getInput();
+            inputs[0] = value;
+            if (value.equals("Q")) {
+                endOfView = true;
+                continue;
+            }
+
+            endOfView = doAction(inputs);
+        } while (!endOfView);
+    }  
+    public String getInput() {
+        Scanner keyboard = new Scanner(System.in);
+        boolean valid = false;
+        String value = null;
+
+        while (!valid) {
+
+            System.out.println("\n" + this.displayMessage);
+            
+            
+            value = keyboard.nextLine();
+            value = value.trim();
+
+            if (value.length() < 1) {
+                System.out.println("\n*** Choose an option ***");
+                continue;
+            }
+            valid = true;
+        }
+        return value;
+    }
     /* N - start new game
     ** S - Save Game
     ** G - Get and resume saved game
