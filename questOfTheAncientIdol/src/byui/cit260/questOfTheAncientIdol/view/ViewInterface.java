a5ef04825c42852a107d82522b856476f11a3d20
Natalie Gibson <35414098+natalierae325@users.noreply.github.com>
2018-03-13 14:01:31 -0600

    View Interface 3/13/18

--- questOfTheAncientIdol/src/byui/cit260/questOfTheAncientIdol/view/ViewInterface.java
+++ questOfTheAncientIdol/src/byui/cit260/questOfTheAncientIdol/view/ViewInterface.java
@@ -9,6 +9,26 @@ package byui.cit260.questOfTheAncientIdol.view;
  *
  * @author natal
  */
-public class ViewInterface {
-    
+public class ViewInterface extends View{
+    @Override
+    public String[] getInputs(){
+        String[] inputs = new String[2];
+        
+        System.out.println("\n The room is starting to flood.");
+        
+        String value = this.getInput("\n Enter the distance you will have to throw the rope in order to (hypotenuse).");
+        if (value.trim().toUpperCase().Equals("Q")){
+            return inputs;
+        }
+        String value = this.getInputs("Enter the ");
+    }
+
+ 
+    }
+
+@Override 
+public boolean doAction(String[] inputs {
+
+double base = Double.parseDouble(inputs[0]));
+double height = Double.parseDouble
 }
