142f685b1bfb9193e4311c3d81d5bd5338f4b48e
abbeymj <35237112+abbeymj@users.noreply.github.com>
2018-03-14 13:15:11 -0600

    view interface class

--- questOfTheAncientIdol/src/viewLayer/ViewInterface.java
+++ questOfTheAncientIdol/src/viewLayer/ViewInterface.java
@@ -1,14 +1,18 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package byui.cit260.questOfTheAncientIdol.view;
+package viewLayer;
 
 /**
  *
  * @author natal
  */
 public class ViewInterface {
+    void display();
+    String[] string getInput(string, )
+
+    
     
 }
