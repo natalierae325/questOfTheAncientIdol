baeb657d29193ad2def8ca00dc8bd70d81a3e8bf
Natalie Gibson <35414098+natalierae325@users.noreply.github.com>
2018-03-27 13:47:49 -0600

    Merge origin/master
    Conflicts:
    	questOfTheAncientIdol/src/viewLayer/ViewInterface.java

--- questOfTheAncientIdol/src/viewLayer/GameControlView.java
+++ questOfTheAncientIdol/src/viewLayer/GameControlView.java
@@ -22,4 +22,21 @@ public class GameControlView {
     
     
 }
- 
+// public static int createNewGame(Player player) {
+//if (player == null)
+//return -1
+//game = create a new Game object
+//Save a reference to the Player object in the game
+//Save a reference to the game in the main class
+//actors = createActors()
+//Save the list of actors in the Game object
+//Assign an actor to the player
+//items = createItems()
+//Save the list of items in the game
+//map = createMap(noOfRows, noOfColumns, items)
+//IF map == null THEN
+// RETURN -1
+//ENDIF
+//Assign the map to the game
+//RETURN 1 // indicates success
+//}
